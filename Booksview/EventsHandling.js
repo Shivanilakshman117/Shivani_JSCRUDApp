f57ae7752ad8632e68f354678ac1eca9f6c0896d
Shivanilakshman117 <52811087+Shivanilakshman117@users.noreply.github.com>
2019-07-21 21:22:03 +0530

    Create EventsHandling.js

---
+++ Booksview/EventsHandling.js
@@ -0,0 +1,29 @@
+var modal = document.querySelector(".modal");
+var infomodal = document.querySelector(".infomodal");
+var trigger = document.querySelector(".trigger");//get button id to toggle modal window
+var DeleteMultiple = document.querySelector(".DeleteMultiple");
+var subtrigger = document.querySelector(".subtrigger");//get button id to toggle modal window
+var closeButton = document.querySelector(".close-button");
+var closeButton2 = document.querySelector(".close-button2");
+
+trigger.addEventListener("click", toggleModal);
+DeleteMultiple.addEventListener("click", DeleteMultiplefn);//call DeleteMultiplefn on click
+subtrigger.addEventListener("click", toggleModal);//toggle modal window
+closeButton.addEventListener("click", toggleModal);
+closeButton2.addEventListener("click", toggleinfoModal);
+window.addEventListener("click", windowOnClick);
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+function toggleinfoModal() {
+    infomodal.classList.toggle("show-infomodal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) 
+        toggleModal();
+        else if(event.target===infomodal)
+        toggleinfoModal();
+}
+
